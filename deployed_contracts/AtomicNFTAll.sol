cb6e5d363701dce8c9a207c0a3309d165e6eb0ba
jaa2 <43010335+jaa2@users.noreply.github.com>
2022-10-03 17:06:19 -0400

    Add deployed AtomicNFT contract

--- deployed_contracts/AtomicNFTAll.sol
+++ deployed_contracts/AtomicNFTAll.sol
@@ -1,4 +1,131 @@
 // SPDX-License-Identifier: MIT
+// File: AtomicNFTImageGenerationSource.sol
+
+
+pragma solidity ^0.8.17;
+
+/**
+ * @dev An "Atomic" NFT contract that requires a proof of complete knowledge from each token recipient.
+ * Generating the same images requires a large `constants.py` file containing the elements.
+ */
+contract AtomicNFTImageGenerationSource {
+    string public sourceCodeCmapUtilsPy = "import random\n\
+from samila.functions import is_valid_color as is_valid_color\n\
+\n\
+def rand_cmap(nlabels, type='bright', verbose=True):\n\
+    \"\"\"\n\
+    Creates a random colormap to be used together with matplotlib. Useful for segmentation tasks\n\
+    :param nlabels: Number of labels (size of colormap)\n\
+    :param type: 'bright' for strong colors, 'soft' for pastel colors\n\
+    :param verbose: Prints the number of labels and shows the colormap. True or False\n\
+    :return: colormap for matplotlib\n\
+    \"\"\"\n\
+    import colorsys\n\
+    import numpy as np\n\
+\n\
+\n\
+    if type not in ('bright', 'soft'):\n\
+        print ('Please choose \"bright\" or \"soft\" for type')\n\
+        return\n\
+\n\
+    if verbose:\n\
+        print('Number of labels: ' + str(nlabels))\n\
+\n\
+    # Generate color map for bright colors, based on hsv\n\
+    if type == 'bright':\n\
+        randHSVcolors = [(np.random.uniform(low=0.0, high=1),\n\
+                          np.random.uniform(low=0.0, high=1),\n\
+                          np.random.uniform(low=0.0, high=1)) for i in range(nlabels)]\n\
+\n\
+        # Convert HSV list to RGB\n\
+        randRGBcolors = []\n\
+        for HSVcolor in randHSVcolors:\n\
+            randRGBcolor = colorsys.hsv_to_rgb(HSVcolor[0], HSVcolor[1], HSVcolor[2])\n\
+            if is_valid_color(randRGBcolor):\n\
+                randRGBcolors.append(randRGBcolor)\n\
+\n\
+\n\
+    # Generate soft pastel colors, by limiting the RGB spectrum\n\
+    if type == 'soft':\n\
+        low = 0.6\n\
+        high = 0.95\n\
+        randRGBcolors = list(filter(is_valid_color, [(np.random.uniform(low=low, high=high) ** 2,\n\
+                          np.random.uniform(low=low, high=high) ** 2,\n\
+                          np.random.uniform(low=low, high=high) ** 2) for i in range(nlabels)]))\n\
+\n\
+\n\
+    for color in randRGBcolors:\n\
+        if len(color) != 3:\n\
+            randRGBcolors.remove(color)\n\
+\n\
+    return randRGBcolors\n\
+\n\
+\n\
+def get_random_cmap(length=20):\n\
+    new_cmap = rand_cmap(int(length/2), type=random.choice(['soft', 'bright']))\n\
+    new_cmap = new_cmap + rand_cmap(int(length/5), type=random.choice(['soft', 'bright']))\n\
+    random.shuffle(new_cmap)\n\
+    return new_cmap\n\
+";
+
+    string public sourceCodeAtomicNftPy = "from PIL import Image\n\
+import math, random\n\
+from samila import GenerativeImage, Projection\n\
+import os\n\
+from cmap_utils import get_random_cmap\n\
+from constants import ELEMENTS\n\
+\n\
+\n\
+def f1(x, y):\n\
+    result = random.uniform(-1,1) * x**2  - math.sin(y**2) + abs(y-x)\n\
+    return result\n\
+\n\
+def f2(x, y):\n\
+    result = random.uniform(-1,1) * y**3 - math.cos(x**2) + 2*x\n\
+    return result\n\
+\n\
+\n\
+def get_image(seeds, save_to_folder, colors):\n\
+    \"\"\" Fully deterministic function for reproducibly generating a single\n\
+        Atomic NFT PNG from a list of random seeds. \"\"\"\n\
+    os.system(\"mkdir -p %s\" % (save_to_folder))\n\
+    open(\"%s/data\" % (save_to_folder), 'w').write(\"%s\\n%s\" % (str(seeds), str(colors)))\n\
+    for seed_index in range(len(seeds)):\n\
+        # Generate component images, one for each seed. Save each component locally.\n\
+        seed = seeds[seed_index]\n\
+        g = GenerativeImage(f1, f2)\n\
+        g.generate(seed=seed)\n\
+        print(\"Using\", colors[seed_index])\n\
+        g.plot(projection=Projection.POLAR, color=colors[seed_index], bgcolor=\"transparent\")\n\
+        g.save_image('%s/seed_%d_%s.png' % (save_to_folder, seed_index, seed), depth=6)\n\
+\n\
+    # Merge components into a single transparent PNG\n\
+    background = Image.open('%s/seed_0_%s.png' % (save_to_folder, seeds[0]))\n\
+    for seed_index in range(1, len(seeds)):\n\
+        foreground = Image.open(\"%s/seed_%d_%s.png\" % (save_to_folder, seed_index, seeds[seed_index]))\n\
+        background.paste(foreground, (0, 0), foreground)\n\
+    background.save(\"%s/final_nft.png\" % (save_to_folder))\n\
+    print(\"Saved complete NFT to %s\" % (save_to_folder))\n\
+\n\
+\n\
+# hardcoded seed = deterministic generation\n\
+main_random = random.Random(69420)\n\
+# generate 1k contracts\n\
+for nft_num in range(0, 200):\n\
+    depth = int(main_random.random() * 8) + 1\n\
+    print(\"Depth\", depth)\n\
+    seeds = []\n\
+    chosen_colors = []\n\
+    for i in range(0, depth):\n\
+        seed = int(10000000000000000000000000000000000000000000000 * main_random.random())\n\
+        seeds.append(seed)\n\
+        chosen_colors.append(random.Random(seed).choice(ELEMENTS))\n\
+        main_random = random.Random(seed)\n\
+    get_image(seeds, \"nfts/atomic_nfts_%d\" % (nft_num), chosen_colors)\n\
+\n\
+    print(\"Finished NFT\", nft_num)\n\
+";
+}
 // File: @openzeppelin/contracts/utils/Counters.sol
 
 
@@ -694,37 +821,6 @@ interface IERC721 is IERC165 {
     function isApprovedForAll(address owner, address operator) external view returns (bool);
 }
 
-// File: @openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol
-
-
-// OpenZeppelin Contracts (last updated v4.5.0) (token/ERC721/extensions/IERC721Enumerable.sol)
-
-pragma solidity ^0.8.0;
-
-
-/**
- * @title ERC-721 Non-Fungible Token Standard, optional enumeration extension
- * @dev See https://eips.ethereum.org/EIPS/eip-721
- */
-interface IERC721Enumerable is IERC721 {
-    /**
-     * @dev Returns the total amount of tokens stored by the contract.
-     */
-    function totalSupply() external view returns (uint256);
-
-    /**
-     * @dev Returns a token ID owned by `owner` at a given `index` of its token list.
-     * Use along with {balanceOf} to enumerate all of ``owner``'s tokens.
-     */
-    function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256);
-
-    /**
-     * @dev Returns a token ID at a given `index` of all the tokens stored by the contract.
-     * Use along with {totalSupply} to enumerate all tokens.
-     */
-    function tokenByIndex(uint256 index) external view returns (uint256);
-}
-
 // File: @openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol
 
 
@@ -1210,171 +1306,6 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
     ) internal virtual {}
 }
 
-// File: @openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol
-
-
-// OpenZeppelin Contracts v4.4.1 (token/ERC721/extensions/ERC721Enumerable.sol)
-
-pragma solidity ^0.8.0;
-
-
-
-/**
- * @dev This implements an optional extension of {ERC721} defined in the EIP that adds
- * enumerability of all the token ids in the contract as well as all token ids owned by each
- * account.
- */
-abstract contract ERC721Enumerable is ERC721, IERC721Enumerable {
-    // Mapping from owner to list of owned token IDs
-    mapping(address => mapping(uint256 => uint256)) private _ownedTokens;
-
-    // Mapping from token ID to index of the owner tokens list
-    mapping(uint256 => uint256) private _ownedTokensIndex;
-
-    // Array with all token ids, used for enumeration
-    uint256[] private _allTokens;
-
-    // Mapping from token id to position in the allTokens array
-    mapping(uint256 => uint256) private _allTokensIndex;
-
-    /**
-     * @dev See {IERC165-supportsInterface}.
-     */
-    function supportsInterface(bytes4 interfaceId) public view virtual override(IERC165, ERC721) returns (bool) {
-        return interfaceId == type(IERC721Enumerable).interfaceId || super.supportsInterface(interfaceId);
-    }
-
-    /**
-     * @dev See {IERC721Enumerable-tokenOfOwnerByIndex}.
-     */
-    function tokenOfOwnerByIndex(address owner, uint256 index) public view virtual override returns (uint256) {
-        require(index < ERC721.balanceOf(owner), "ERC721Enumerable: owner index out of bounds");
-        return _ownedTokens[owner][index];
-    }
-
-    /**
-     * @dev See {IERC721Enumerable-totalSupply}.
-     */
-    function totalSupply() public view virtual override returns (uint256) {
-        return _allTokens.length;
-    }
-
-    /**
-     * @dev See {IERC721Enumerable-tokenByIndex}.
-     */
-    function tokenByIndex(uint256 index) public view virtual override returns (uint256) {
-        require(index < ERC721Enumerable.totalSupply(), "ERC721Enumerable: global index out of bounds");
-        return _allTokens[index];
-    }
-
-    /**
-     * @dev Hook that is called before any token transfer. This includes minting
-     * and burning.
-     *
-     * Calling conditions:
-     *
-     * - When `from` and `to` are both non-zero, ``from``'s `tokenId` will be
-     * transferred to `to`.
-     * - When `from` is zero, `tokenId` will be minted for `to`.
-     * - When `to` is zero, ``from``'s `tokenId` will be burned.
-     * - `from` cannot be the zero address.
-     * - `to` cannot be the zero address.
-     *
-     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
-     */
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal virtual override {
-        super._beforeTokenTransfer(from, to, tokenId);
-
-        if (from == address(0)) {
-            _addTokenToAllTokensEnumeration(tokenId);
-        } else if (from != to) {
-            _removeTokenFromOwnerEnumeration(from, tokenId);
-        }
-        if (to == address(0)) {
-            _removeTokenFromAllTokensEnumeration(tokenId);
-        } else if (to != from) {
-            _addTokenToOwnerEnumeration(to, tokenId);
-        }
-    }
-
-    /**
-     * @dev Private function to add a token to this extension's ownership-tracking data structures.
-     * @param to address representing the new owner of the given token ID
-     * @param tokenId uint256 ID of the token to be added to the tokens list of the given address
-     */
-    function _addTokenToOwnerEnumeration(address to, uint256 tokenId) private {
-        uint256 length = ERC721.balanceOf(to);
-        _ownedTokens[to][length] = tokenId;
-        _ownedTokensIndex[tokenId] = length;
-    }
-
-    /**
-     * @dev Private function to add a token to this extension's token tracking data structures.
-     * @param tokenId uint256 ID of the token to be added to the tokens list
-     */
-    function _addTokenToAllTokensEnumeration(uint256 tokenId) private {
-        _allTokensIndex[tokenId] = _allTokens.length;
-        _allTokens.push(tokenId);
-    }
-
-    /**
-     * @dev Private function to remove a token from this extension's ownership-tracking data structures. Note that
-     * while the token is not assigned a new owner, the `_ownedTokensIndex` mapping is _not_ updated: this allows for
-     * gas optimizations e.g. when performing a transfer operation (avoiding double writes).
-     * This has O(1) time complexity, but alters the order of the _ownedTokens array.
-     * @param from address representing the previous owner of the given token ID
-     * @param tokenId uint256 ID of the token to be removed from the tokens list of the given address
-     */
-    function _removeTokenFromOwnerEnumeration(address from, uint256 tokenId) private {
-        // To prevent a gap in from's tokens array, we store the last token in the index of the token to delete, and
-        // then delete the last slot (swap and pop).
-
-        uint256 lastTokenIndex = ERC721.balanceOf(from) - 1;
-        uint256 tokenIndex = _ownedTokensIndex[tokenId];
-
-        // When the token to delete is the last token, the swap operation is unnecessary
-        if (tokenIndex != lastTokenIndex) {
-            uint256 lastTokenId = _ownedTokens[from][lastTokenIndex];
-
-            _ownedTokens[from][tokenIndex] = lastTokenId; // Move the last token to the slot of the to-delete token
-            _ownedTokensIndex[lastTokenId] = tokenIndex; // Update the moved token's index
-        }
-
-        // This also deletes the contents at the last position of the array
-        delete _ownedTokensIndex[tokenId];
-        delete _ownedTokens[from][lastTokenIndex];
-    }
-
-    /**
-     * @dev Private function to remove a token from this extension's token tracking data structures.
-     * This has O(1) time complexity, but alters the order of the _allTokens array.
-     * @param tokenId uint256 ID of the token to be removed from the tokens list
-     */
-    function _removeTokenFromAllTokensEnumeration(uint256 tokenId) private {
-        // To prevent a gap in the tokens array, we store the last token in the index of the token to delete, and
-        // then delete the last slot (swap and pop).
-
-        uint256 lastTokenIndex = _allTokens.length - 1;
-        uint256 tokenIndex = _allTokensIndex[tokenId];
-
-        // When the token to delete is the last token, the swap operation is unnecessary. However, since this occurs so
-        // rarely (when the last minted token is burnt) that we still do the swap here to avoid the gas cost of adding
-        // an 'if' statement (like in _removeTokenFromOwnerEnumeration)
-        uint256 lastTokenId = _allTokens[lastTokenIndex];
-
-        _allTokens[tokenIndex] = lastTokenId; // Move the last token to the slot of the to-delete token
-        _allTokensIndex[lastTokenId] = tokenIndex; // Update the moved token's index
-
-        // This also deletes the contents at the last position of the array
-        delete _allTokensIndex[tokenId];
-        _allTokens.pop();
-    }
-}
-
 // File: AtomicNFT.sol
 
 
@@ -1392,13 +1323,30 @@ interface ICKRegistry {
 /**
  * @dev An "Atomic" NFT contract that requires a proof of complete knowledge from each token recipient.
  */
-contract AtomicNFT is ERC721, ERC721Enumerable, Ownable {
+contract AtomicNFT is ERC721, Ownable, AtomicNFTImageGenerationSource {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
+    
+    /**
+     * @dev The registry contract that determines if an address has provided
+     * a proof of complete knowledges
+     */
     ICKRegistry public immutable ckRegistry;
     
+    /**
+     * @dev A fee on each open mint as a deterrant for scalping
+     */
     uint256 public mintFee;
+    
+    /**
+     * @dev If enabled, addresses with a proof of complete knowledge will
+     * be able to mint NFTs using the `mint` function.
+     */
     bool public openMintingEnabled = false;
+    
+    /**
+     * @dev The total collection size after all NFTs have been minted
+     */
     uint256 public immutable collectionSize;
     
     constructor(ICKRegistry _ckRegistry, uint256 _collectionSize, uint256 _mintFee) ERC721("Atoms", "ATM") {
@@ -1488,16 +1436,8 @@ contract AtomicNFT is ERC721, ERC721Enumerable, Ownable {
         address from,
         address to,
         uint256 tokenId
-    ) internal override(ERC721, ERC721Enumerable) {
+    ) internal override(ERC721) {
         super._beforeTokenTransfer(from, to, tokenId);
         require(ckRegistry.isCK(to), "Recipient needs a CK proof");
-    }
-    
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool) {
-        return super.supportsInterface(interfaceId);
     }
 }
